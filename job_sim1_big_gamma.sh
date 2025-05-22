5e510720559a755a6a96ed8392d1eaebcf824536
Valentin Belardi <valentin.belardi@epfl.ch>
2025-05-22 15:12:55 +0200

    fix

--- job_sim1_big_gamma.sh
+++ job_sim1_big_gamma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=sim1_small_gamma
-#SBATCH --output=log.out_sim1_small_gamma
-#SBATCH --error=log.err_sim1_small_gamma
+#SBATCH --job-name=sim1_big_gamma
+#SBATCH --output=log.out_sim1_big_gamma
+#SBATCH --error=log.err_sim1_big_gamma
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=20
